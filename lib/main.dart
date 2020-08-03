c5efffa4687e9ad7936a36d98ec271255e95a702
Shubhanshu Saxena <54344426+shubhanshu02@users.noreply.github.com>
2020-08-03 19:14:39 +0530

    CodeLabs Part 1 Complete

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final word_pair = WordPair.random();
+    return MaterialApp(
+      title: "Startup Name Generator",
+      home: RandomWords(),
+    );
+  }
+}
+
+
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: buildSuggestions(),
+    );
+    final word_pair = WordPair.random();
+    return Text(word_pair.first + " " + word_pair.second);
+  }
+  Widget buildSuggestions() {
+    return ListView.builder(
+      padding: EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return Divider();
+      
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.first + " " + pair.second,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
